ae24345786c66a666d3bd8bcd37b773ef70c5d75
soyalexokei <alex.audiovisual.engineer@gmail.com>
2023-11-19 18:13:22 +0100

    4º commit

--- assets/js/hobbies.js
+++ assets/js/hobbies.js
@@ -1,44 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
+const screen = document.getElementById('pantalla');
 
-    const screen = document.getElementById('pantalla');
-    const contexto = canvas.getContext("2d");
+//-- Definir el tamaño del canvas.
+screen.width = 280;
+screen.height = 492;
 
-    //-- Definir el tamaño del canvas.
-    screen.width = 256;
-    screen.height = 446;
+//-- Obtener el contexto del canvas
+const contexto = screen.getContext("2d");
 
-    // Lista de URLs de imágenes
-    var imageUrls = [
-        "../img/hobbies/no-signal.png",
-        "../img/hobbies/nike.png"
-    ];
+// Lista de URLs de imágenes
+var imageUrls = [
+    "../img/hobbies/no-signal.png",
+    "../img/hobbies/nike.png"
+];
 
-    // Cargar imágenes
-    var images = [];
-    var cont = 0;
+//-- Posición del elemento a animar
+let x = 0;
+let y = 0;
 
-    function loadImages() {
-        for (var i = 0; i < imageUrls.length; i++) {
-            var img = new Image();
-            img.src = imageUrls[i];
-            images.push(img);
-        }
+//-- Velocidad horizontal del objeto
+let velx = 3;
+let vely = 1.75;
+
+//-- Cte de las medidas del objeto a mover.
+const anchoObj = 20;
+const altoObj = 20;
+
+//-- Funcion principal de animacion
+function update() 
+{
+    console.log("test");
+    //-- Algoritmo de animacion:
+    //-- 1) Actualizar posicion del  elemento
+    //-- (física del movimiento rectilineo uniforme)
+
+    //-- Condicion de rebote en extremos del canvas
+    if (x < 0 || x >= (screen.width - anchoObj) ) {
+        velx = -velx;
+    }
+    if (y < 0 || y >= (screen.height - altoObj)) {
+        vely = -vely;
     }
 
-    // Dibujar la imagen actual en el canvas
-    function drawImage() {
-        contexto.clearRect(0, 0, screen.width, screen.height);
-        contexto.drawImage(images[cont], 0, 0, screen.width, screen.height);
-    }
+    //-- Actualizar la posición
+    x = x + velx;
+    y = y + vely;
 
-    // Cambiar a la siguiente imagen cada segundo
-    function changeImage() {
-        cont = (cont + 1) % images.length;
-        drawImage();
-    }
+    //-- 2) Borrar el canvas
+    contexto.clearRect(0, 0, screen.width, screen.height);
 
-    // Cargar imágenes y comenzar la animación
-    loadImages();
-    drawImage();
-    setInterval(changeImage, 1000);
-});
+    //-- 3) Dibujar los elementos visibles
+    contexto.beginPath();
+    contexto.rect(x, y, anchoObj, altoObj);
+
+    //-- Dibujar
+    contexto.fillStyle = 'red';
+
+    //-- Rellenar
+    contexto.fill();
+
+    //-- Dibujar el trazo
+    contexto.stroke()
+    contexto.closePath();
+
+    //-- 4) Volver a ejecutar update cuando toque
+    requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
