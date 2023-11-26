98c13d57985360ff7ea82725dcb221e7e8825423
soyalexokei <alex.audiovisual.engineer@gmail.com>
2023-11-26 13:47:31 +0100

    5º commit

--- assets/js/hobbies.js
+++ assets/js/hobbies.js
@@ -1,70 +1,25 @@
-const screen = document.getElementById('pantalla');
+const space = document.getElementById('pantalla');
 
 //-- Definir el tamaño del canvas.
-screen.width = 280;
-screen.height = 492;
+space.width = 280;
+space.height = 492;
 
 //-- Obtener el contexto del canvas
-const contexto = screen.getContext("2d");
+const contexto = space.getContext("2d");
 
 // Lista de URLs de imágenes
-var imageUrls = [
-    "../img/hobbies/no-signal.png",
-    "../img/hobbies/nike.png"
-];
+const imagen = new Image();
+imagen.src = 'hobbies/no-signal.png';
 
-//-- Posición del elemento a animar
-let x = 0;
-let y = 0;
+imagen.onload = function () {
 
-//-- Velocidad horizontal del objeto
-let velx = 3;
-let vely = 1.75;
+    function update() {
 
-//-- Cte de las medidas del objeto a mover.
-const anchoObj = 20;
-const altoObj = 20;
+        contexto.clearRect(0, 0, space.width, space.height);
+        contexto.drawImage(imagen, 10, 10, 50, 150);
+        requestAnimationFrame(update);
 
-//-- Funcion principal de animacion
-function update() 
-{
-    console.log("test");
-    //-- Algoritmo de animacion:
-    //-- 1) Actualizar posicion del  elemento
-    //-- (física del movimiento rectilineo uniforme)
-
-    //-- Condicion de rebote en extremos del canvas
-    if (x < 0 || x >= (screen.width - anchoObj) ) {
-        velx = -velx;
-    }
-    if (y < 0 || y >= (screen.height - altoObj)) {
-        vely = -vely;
     }
 
-    //-- Actualizar la posición
-    x = x + velx;
-    y = y + vely;
-
-    //-- 2) Borrar el canvas
-    contexto.clearRect(0, 0, screen.width, screen.height);
-
-    //-- 3) Dibujar los elementos visibles
-    contexto.beginPath();
-    contexto.rect(x, y, anchoObj, altoObj);
-
-    //-- Dibujar
-    contexto.fillStyle = 'red';
-
-    //-- Rellenar
-    contexto.fill();
-
-    //-- Dibujar el trazo
-    contexto.stroke()
-    contexto.closePath();
-
-    //-- 4) Volver a ejecutar update cuando toque
-    requestAnimationFrame(update);
-}
-
-//-- ¡Que empiece la función!
-update();
+    update();
+};
